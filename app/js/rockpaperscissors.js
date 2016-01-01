49b7e52ce3d3caa22fcfdb9d806587bc3f646e7b
Jehnean Jablonski <jehneanj@toyboxmedia.com>
2015-12-31 17:27:28 -0800

    Added Unit 4 Homework

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -54,7 +54,33 @@
         // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
         // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
         /* YOUR CODE HERE */
-        return winner;
+
+        if (playerMove === computerMove) {
+            winner = 'tie';
+
+        } else if (playerMove === 'rock') {
+            if (computerMove === "scissors") {
+                winner = 'player';
+            } else {
+                winner = 'computer';
+            }
+
+        } else if (playerMove === 'scissors') {
+            if (computerMove === 'paper') {
+                winner = 'player';
+            } else {
+                winner = 'computer';
+            }
+        } else if (playerMove === "paper") {
+            if (computerMove === "rock") {
+                winner = 'player';
+            } else {
+                winner = 'computer';
+            }
+        }
+
+        console.log(compare(userChoice, computerChoice));
+
     }
 
     function playToFive() {
